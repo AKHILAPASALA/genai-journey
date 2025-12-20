a4c6448161fc0503476ecafe24585c6ffde5e73e
akki <akhilapasala829@gmail.com>
2025-12-20 13:04:49 +0530

    storing all python basics practice code

--- basics_practice.py
+++ basics_practice.py
@@ -1,1 +1,12 @@
-print("sum of a and b is",5)
+a=int(input("Enter a value: ")) #reading random input integer numbers
+b=int(input("Enter b value: "))
+print("The sum of a and b is {}".format(a+b)) #performed mathematical operation '+" and also used .format concept here to print the sum
+print("product of a and b is {}".format(a*b))
+print("the difference of a and b is {}".format(a-b))
+print("The division of a and b is {}".format(a/b))
+print("The modular division of a and b is {}".format(a%b))
+
+print("Enter the names list: ",end=" ")
+l=list(map(str, input().split(" ")))
+print(l)
+
--- helloworld.py
+++
@@ -1,2 +0,0 @@
-print("Hello this is my first step to learn AI course journey!!")
-print("welcome to the learning program")
